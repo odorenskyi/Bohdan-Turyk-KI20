264b34d0beae3942625799c09cce240ca27ca794
Turik Bohdan <bohdanturik@gmail.com>
2021-04-28 11:07:44 +0300

    worked SoftWare

--- lab08/VisualStudio/test/Header.h
+++
@@ -1,9 +0,0 @@
-#ifndef HEADER_H
-#define HEADER_H
-
-char name();
-int calc();
-int s_calculation();
-
-#endif // !HEADER_H
-
--- lab08/prj/BMTP8.2lib/bmtp82lib1/main.cpp
+++ lab08/prj/BMTP8.2lib/bmtp82lib1/main.cpp
@@ -1,5 +1,6 @@
 #include <iostream>
 #include <math.h>
+#include "lib82.h"
 
 using namespace std;
 
